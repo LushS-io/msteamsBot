15e8010ef2242d81a7ef5d991f19461ce80ef3c7
Troy <troykirin@gmail.com>
2020-08-09 11:34:34 -0700

    add email to user profile

--- storage/user_profile.py
+++ storage/user_profile.py
@@ -1,4 +1,5 @@
 class UserProfile:
     # init to None as default value
-    def __init__(self, name: str = None):
+    def __init__(self, name: str = None, email: str = None):
         self.name = name
+        self.email = email
